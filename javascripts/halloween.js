0691268659d268993c1d411d936a656317e2831a
Santos López Lozano <santos10502@gmail.com>
2020-11-08 16:55:48 +0100

    Add files via upload

---
+++ javascripts/halloween.js
@@ -0,0 +1,13 @@
+
+function mostrar(identificador){
+    document.getElementById(identificador).style.visibility="visible";
+}
+function ocultar(identificador){
+    document.getElementById(identificador).style.visibility="hidden";
+}
+function sonar(sonido){
+    document.getElementById(sonido).play();
+}
+function parar(sonido){
+    document.getElementById(sonido).pause();
+}
